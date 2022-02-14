2e75e3539efe3dcbdaa81e9560065a41fa1846b9
Antonio Sabino <aslimajr@outlook.com>
2022-02-14 18:52:22 -0300

    Requisito 02 token salvo

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import user from './player';
+import token from './token';
 
 const rootReducer = combineReducers({
   user,
+  token,
 });
 
 export default rootReducer;
