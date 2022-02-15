b71819fcd34c4d05945b4c552461e9f94e59294f
Antonio Sabino <aslimajr@outlook.com>
2022-02-15 17:43:59 -0300

    5 nao resolvida

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Answer from '../Component/Answer';
 import Header from '../Component/Hearder';
 
 class Game extends Component {
   render() {
+    const { history } = this.props;
     return (
-      <Header />
+      <section>
+        <Header />
+        <Answer history={ history } />
+      </section>
+
     );
   }
 }
+
+Game.propTypes = {
+  history: PropTypes.objectOf(PropTypes.any),
+}.isRequired;
 
 export default Game;
